87b49faec08a1d424ff7ae300b66fd0891869c56
Ray <ray.wang@nexsys.com.tw>
2023-05-15 17:56:02 +0800

    TEST

---
+++ 1204ConsoleApp/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+RomanToInt("MCMXCIV");
+
+
+
+
+static int RomanToInt(string s)
+{
+    int num = 0;
+    int total = 0;
+    int minus = 0;
+    for (int i = 0; i < s.Length; i++)
+    {
+        switch (s[i])
+        {
+            case 'I':
+                num = 1;
+                break;
+            case 'V':
+                num = 5;
+                break;
+            case 'X':
+                num = 10;
+                break;
+            case 'L':
+                num = 50;
+                break;
+            case 'C':
+                num = 100;
+                break;
+            case 'D':
+                num = 500;
+                break;
+            case 'M':
+                num = 1000;
+                break;
+        }
+        total += num;
+        if ( i < s.Length-1)
+        {
+            string test = s[i] + s[i + 1].ToString();
+            switch (s[i] + s[i + 1].ToString())
+            {
+                case "IV":
+                    minus = 2;
+                    break;
+                case "IX":
+                    minus = 2;
+                    break;
+                case "XL":
+                    minus = 20;
+                    break;
+                case "XC":
+                    minus = 20;
+                    break;
+                case "CD":
+                    minus = 200;
+                    break;
+                case "CM":
+                    minus = 200;
+                    break;
+                default: minus = 0;
+                    break;
+
+            }
+            total -= minus;
+        }
+    }
+    return total;
+}
